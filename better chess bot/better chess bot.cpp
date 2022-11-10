5a0db0381b565485bf6bad8084debcab6cbcd96e
Jonathan Braver <enatanb@gmail.com>
2022-11-10 10:20:56 +0200

    added bishop and rook observation

--- better chess bot/better chess bot.cpp
+++ better chess bot/better chess bot.cpp
@@ -87,6 +87,8 @@ B64 king_moves[64];
 B64 knight_moves[64];
 B64 pawn_moves[64*2]; // the 2 colors move in opposite direction
 B64 pawn_attacks[64*2]; // further conditional split
+// rooks always have 14 potential moves, 7 horizontal and 7 vertical
+// bishops can have as little as 7 moves and up to 13 (corner and center respectivly)
 
 void prepare_king_moves() {
     B64 current_board = 1ULL;
