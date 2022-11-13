934e701c892c3dd03ab893c7e5912d0fa23a8942
yoniinoy <enatanb@gmail.com>
2022-11-13 14:06:58 +0200

    changed lowerst bit search to use a 64 bit De Bruijn Sequence

--- better chess bot/Boad operations.cpp
+++ better chess bot/Boad operations.cpp
@@ -14,7 +14,7 @@ uint8_t lowestBitIndex(uint32_t v) {
 }
 
 // finding the index of the least significant set bit in a 64 bit number
-uint8_t lowestBitIndex64(uint64_t v) {
+uint8_t lowestBitIndex32_2(uint64_t v) {
     // splitting the number into 2 words to use the De Bruijn Sequence on both halfs
 
     uint8_t ans = lowestBitIndex(v); // cast down to 32 first bits
@@ -27,4 +27,18 @@ uint8_t lowestBitIndex64(uint64_t v) {
     }
 
     return ans;
+}
+
+// finding the index of the least significant set bit in a 64 bit long word
+// De Bruijn Sequence
+uint8_t lowestBitIndex64(uint64_t v) {
+    static const uint8_t BitPositionLookup[64] = // hash table
+    {
+      0, 1, 2, 4, 8, 17, 34, 5, 11, 23, 47, 31, 63, 62, 61, 59,
+      55, 46, 29, 58, 53, 43, 22, 44, 24, 49, 35, 7, 15, 30, 60, 57,
+      51, 38, 12, 25, 50, 36, 9, 18, 37, 10, 21, 42, 20, 41, 19, 39,
+      14, 28, 56, 48, 33, 3, 6, 13, 27, 54, 45, 26, 52, 40, 16, 32
+    };
+    // using the De Bruijn Sequence and lookink at the last 6 bits
+    return BitPositionLookup[((uint64_t)((v & -v) * 0x022fdd63cc95386dU)) >> 58];
 }
--- better chess bot/Board operation.h
+++ better chess bot/Board operation.h
@@ -3,4 +3,5 @@
 #include <cstdint>
 
 uint8_t lowestBitIndex(uint32_t v);
+uint8_t lowestBitIndex32_2(uint64_t v);
 uint8_t lowestBitIndex64(uint64_t v);
