4f263f1674e2edb3d2367ec30d2cdcb4a5f76ab6
yoniinoy <enatanb@gmail.com>
2022-11-24 16:32:17 +0200

    added basic eval

--- better chess bot/Evaluation.cpp
+++ better chess bot/Evaluation.cpp
@@ -6,7 +6,7 @@
 idea dump :
 
 open king - each attack 'lane' (row,col,diagonal) without cover is + for the otehr side
-bishop supiriority - bonus for having both and discoraging 'equal' trades vs knights
+bishop supiriority - bonus for having both and discoraging 'equal' trades vs knights (done)
 penatly for stacked pawns - multiple pawns on the same columb = bad bad
 bonus for high queen activity near the enemy king / side of the board
 bonus for attacking more times than the square is defended
@@ -19,7 +19,7 @@ bonus for passed pawns
 bonus for outposts (pieces coverd by pawns that can't be kicked out by enemy pawns)
 bonus for total area avalible for pieces to move into and/or standing on center tiles (chould be dangerous in endgames, redundant with good moves?)
 bonus for rooks on open/semi open columns
-bonus for knights in closed positions, bonus for bishops n open positions (again, bishiop supiriority)
+bonus for knights in closed positions, bonus for bishops in open positions (again, bishiop supiriority)
 bonus/penalty for bishop based on pawn color (white bishop bonus if 12 pawns are on dark somehow)
 
 make everything as dynamic and generic as possible to allow for many, MANY playstyles for the bot
@@ -34,8 +34,8 @@ inline bool insufficient_material(BoardPosition position) {
 
     bool low_material = false;
 
-    int white_knights, white_bishops;
-    int black_knights, black_bishops;
+    int white_bishops, white_knights;
+    int black_bishops, black_knights;
 
     // potential return guard refactor here
     if ( !(position.white_pawns || position.black_pawns || // where there's a pawn theres (sometimes) a way
@@ -43,11 +43,11 @@ inline bool insufficient_material(BoardPosition position) {
            position.white_queens || position.black_queens)) { // rook endgames are more likly than queen endgames
         
         // saving counts to avoit countign each time, other wise i'd just do a MASSIVE return
-        white_knights = count_bits64(position.white_knights);
         white_bishops = count_bits64(position.white_bishops);
+        white_knights = count_bits64(position.white_knights);
 
-        black_knights = count_bits64(position.black_knights);
         black_bishops = count_bits64(position.black_bishops);
+        black_knights = count_bits64(position.black_knights);
 
 
         // this CAN be rearanged with ifs, cutting off conflicting cases but having SOME readabiliy is prefered
@@ -55,18 +55,18 @@ inline bool insufficient_material(BoardPosition position) {
                        (white_bishops >= 2 || black_bishops >= 2) || // a mate is possible with 2 bishops
                        (white_knights >= 3 || black_knights >= 3) || // this... is mate. but why? why would you do this to yourself?
                          ( // seperated for easier order changes ;)
-                             ( white_knights + white_bishops == 1    && !(black_knights | black_bishops)) ||   // white has only bishop or knight
-                             ( !(white_knights | white_bishops)      && black_knights + black_bishops == 1) || // black has only bishop or knight
-                             ( white_knights + white_bishops == 1    && black_knights + black_bishops == 1) || // both have bishop or knight
-                             ( (white_knights == 2 && !white_bishops) && !(black_knights | black_bishops)) ||   // white has only 2 knights
-                             ( !(white_knights | white_bishops)      && (black_knights == 2 && !black_bishops)) // black has only 2 knights
+                             ( white_knights + white_bishops == 1     && !(black_knights | black_bishops)) ||    // white has only bishop or knight
+                             ( !(white_knights | white_bishops)       && black_knights + black_bishops == 1) ||  // black has only bishop or knight
+                             ( white_knights + white_bishops == 1     && black_knights + black_bishops == 1) ||  // both have bishop or knight
+                             ( (white_knights == 2 && !white_bishops) && !(black_knights | black_bishops)) ||    // white has only 2 knights
+                             ( !(white_knights | white_bishops)       && (black_knights == 2 && !black_bishops)) // black has only 2 knights
                          );
     }
 
     return low_material;
 }
 
-bool check_draw(GameState& current_state) {
+bool is_draw(GameState& current_state) {
     GameState& original_state = current_state;
     bool is_draw = false;
     int repetion_count = 0;
@@ -86,4 +86,27 @@ bool check_draw(GameState& current_state) {
     }
 
     return is_draw;
+}
+
+int material_eval(BoardPosition position) {
+    int white_queens, white_rooks, white_bishops, white_knights, white_pawns;
+    int black_queens, black_rooks, black_bishops, black_knights, black_pawns;
+
+    white_queens  = count_bits64(position.white_queens);
+    white_rooks   = count_bits64(position.white_rooks);
+    white_bishops = count_bits64(position.white_bishops);
+    white_knights = count_bits64(position.white_knights);
+    white_pawns   = count_bits64(position.white_pawns);
+
+    black_queens  = count_bits64(position.black_queens);
+    black_rooks   = count_bits64(position.black_rooks);
+    black_bishops = count_bits64(position.black_bishops);
+    black_knights = count_bits64(position.black_knights);
+    black_pawns   = count_bits64(position.black_pawns);
+
+    return (white_queens  - black_queens) * QUEEN_VALUE +
+           (white_rooks   - black_rooks) * ROOK_VALUE +
+           (white_bishops - black_bishops) * BISHOP_VALUE +
+           (white_knights - black_knights) * KNIGHT_VALUE +
+           (white_pawns   - black_pawns) * PAWN_VALUE;
 }
--- better chess bot/Evaluation.h
+++ better chess bot/Evaluation.h
@@ -5,11 +5,14 @@
 const int DRAW_MOVES = 50;
 const int DRAW_REPETITIONS = 3;
 
-const int PAWN_VALUE = 1;
-const int KNIGHT_VALUE = 3;
-const int BISHOP_VALUE = 3; // debatable, but its 'officially' worth 3 points.
-const int ROOK_VALUE = 5;
-const int QUEEN_VALUE = 9;
+// trying to avoind floats during computation (but also avoid overflow)
+const int PAWN_VALUE = 100;
+const int KNIGHT_VALUE = 310; // so the bot dosent want to go rook + pawn for 2 knights/bishops
+const int BISHOP_VALUE = 320; // slightly over a knight
+const int ROOK_VALUE = 500;
+const int QUEEN_VALUE = 900;
+const int WIN_VALUE = 100000; // aka 'stupidly large number' that you can't achive even with 64 queens
+const int DRAW_VALUE = 0;
 
-
-bool check_draw(GameState& current_state);
+bool is_draw(GameState& current_state);
+int material_eval(BoardPosition position);
