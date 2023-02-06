6684306c839a7d869e3577a6e078a1b401d2d995
yoniinoy <enatanb@gmail.com>
2023-02-06 17:03:14 +0200

    starting position generation AGAIN Moves are impractical to create as they require insight from the resulting state. I decided to leave them in while completly baypassing them for the position generation.

--- better chess bot/Move generation.cpp
+++ better chess bot/Move generation.cpp
@@ -1,6 +1,5 @@
 
 #include "Move generation.h"
-#include "Board operation.h"
 
 B64 generate_bishop_moves(const B64 all_pieces, B64 piece) {
 
@@ -108,4 +107,88 @@ void prepare_pawn_moves() {
     // if a pawn has no POTENTIAL moves, he promoted
     prepare_white_pawn_moves();
     prepare_black_pawn_moves();
+}
+
+
+void possible_simple_positions(std::vector<BoardPosition> positions, const BoardPosition position, const PlayerColor color, const B64 pieces, const B64 blockers, const B64 valid_destinations, B64(*move_generator)(B64, B64), const B64* move_source, const int index_scale, const int first_index) {
+    std::vector<B64> single_pieces;                                                                                                                                                                   
+    std::vector<B64> destinations;                                                                                                                                                                     
+    BoardPosition new_position = {};                                                                                                                                                                  
+    B64 potential_moves = 0;    
+    const bool is_white = color == WHITE;
+
+    // shorthand enemy pieces
+    const B64* enemy_pawns = (is_white ? &new_position.black_pawns : &new_position.white_pawns);
+    const B64* enemy_knights = (is_white ? &new_position.black_knights : &new_position.white_knights);
+    const B64* enemy_bishops = (is_white ? &new_position.black_bishops : &new_position.white_bishops);
+    const B64* enemy_rooks = (is_white ? &new_position.black_rooks : &new_position.white_rooks);
+    const B64* enemy_queens = (is_white ? &new_position.black_queens : &new_position.white_queens);
+    const B64* enemy_king = (is_white ? &new_position.black_king : &new_position.white_king);
+
+    seperate_bits(pieces, single_pieces); // get a vector of all pieces
+    positions.reserve(count_bits64(pieces) * MAX_QUEEN_MOVES); // streach the vector to the worst case
+    for (B64 piece : single_pieces) {
+        new_position = position; // reset the new position
+        if (move_generator != nullptr) {
+            potential_moves = move_generator(blockers, pieces) & valid_destinations; // get the valid moves per piece
+        } else {
+            potential_moves = move_source[first_index + index_scale * lowestBitIndex64_s(piece)];
+        }
+        seperate_bits(piece, destinations); // seperate the generated moves
+        for (B64 move : destinations) {
+            // delete any enemy piece in the destination
+            clear_bit(*enemy_pawns, move);
+            clear_bit(*enemy_knights, move);
+            clear_bit(*enemy_bishops, move);
+            clear_bit(*enemy_rooks, move);
+            clear_bit(*enemy_queens, move);
+            positions.push_back(new_position);
+        }
+    }
+}
+
+std::vector<BoardPosition> possible_positions(const BoardPosition position, const PlayerColor color) {
+    std::vector<BoardPosition> positions;
+    std::vector<B64> pieces;
+    std::vector<B64> destinations;
+    BoardPosition new_position = position;
+    B64 potential_moves = 0;
+    const bool is_white = color == WHITE;
+    const B64 blockers = position.white | position.black;
+    const B64 not_own = (is_white ? ~position.white : ~position.black); // valid destinations
+
+    // shorthand own pieces
+    const B64 pawns = (is_white ? position.white_pawns : position.black_pawns);
+    const B64 knights = (is_white ? position.white_knights : position.black_knights);
+    const B64 bishops = (is_white ? position.white_bishops : position.black_bishops);
+    const B64 rooks = (is_white ? position.white_rooks : position.black_rooks);
+    const B64 queens = (is_white ? position.white_queens : position.black_queens);
+    const B64 king = (is_white ? position.white_king : position.black_king);
+
+    if (queens != 0) {
+        possible_simple_positions(positions, position, color, queens, blockers, not_own, &generate_queen_moves);
+    }
+    if (rooks != 0) {
+        possible_simple_positions(positions, position, color, queens, blockers, not_own, &generate_rook_moves);
+    }
+    if (bishops != 0) {
+        possible_simple_positions(positions, position, color, queens, blockers, not_own, &generate_bishop_moves);
+    }
+    if (knights != 0) {
+        possible_simple_positions(positions, position, color, queens, blockers, not_own, nullptr, knight_moves);
+    }
+
+    // king nornals
+    possible_simple_positions(positions, position, color, queens, blockers, not_own, nullptr, king_moves);
+
+    // need non kil pawn moves
+    // need castling
+    // need pawn kills
+    // need en passant pawn kills
+
+    return positions;
+}
+
+inline std::vector<BoardPosition> possible_positions(const GameState state) {
+    return possible_positions(state.position, (state.turn % 2 == 0 ? WHITE : BLACK));
 }
--- better chess bot/Move generation.h
+++ better chess bot/Move generation.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "Board structure.h"
+#include "Board operation.h"
+#include "Move handling.h"
 
 static B64 king_moves[64];
 static B64 knight_moves[64];
 static B64 pawn_moves[64 * 2]; // the 2 colors move in opposite direction
 static B64 pawn_attacks[64 * 2]; // further conditional split
+
+constexpr int MAX_ROOK_MOVES = 14;
+constexpr int MAX_BISHOP_MOVES = 13;
+constexpr int MAX_QUEEN_MOVES = MAX_ROOK_MOVES + MAX_BISHOP_MOVES;
+
 // rooks always have 14 potential moves, 7 horizontal and 7 vertical
 // bishops can have as little as 7 moves and up to 13 (corner and center respectivly)
 
@@ -20,3 +27,5 @@ void prepare_knight_moves();
 void prepare_white_pawn_moves();
 void prepare_black_pawn_moves();
 void prepare_pawn_moves();
+
+void possible_simple_positions(std::vector<BoardPosition> positions, const BoardPosition position, const PlayerColor color, const B64 pieces, const B64 blockers, const B64 valid_destinations, B64(*move_generator)(B64, B64) = nullptr, const B64* move_source = nullptr, const int index_scale = 1, const int first_index = 0);
