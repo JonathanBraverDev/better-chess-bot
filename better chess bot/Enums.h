a6f3a9f5cce978c8d85aacd11d4f90cefdd7a43c
Jonathan Braver <enatanb@gmail.com>
2023-11-01 11:45:58 +0200

    added attacker enum

--- better chess bot/Enums.h
+++ better chess bot/Enums.h
@@ -2,5 +2,6 @@
 
 enum class Color { WHITE, BLACK };
 enum class PieceType { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
+enum class AttackerType { KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN }; // inverted value for capturing piece
 
 enum class MoveType { NORMAL, PAWN_UNIQE, CASTLE_LONG, CASTLE_SHORT };
