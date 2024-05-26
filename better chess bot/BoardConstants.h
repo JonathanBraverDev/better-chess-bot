f43781be7ec1569855c08e20d29d9481c6dbc388
Jonathan Braver <enatanb@gmail.com>
2024-05-26 11:17:47 +0300

    changed castling related consts

--- better chess bot/BoardConstants.h
+++ better chess bot/BoardConstants.h
@@ -164,15 +164,21 @@ constexpr B64 TILE_BLACK = 0xAAAAAAAAAAAAAAAAULL;
 constexpr B64 TILE_WHITE = 0x5555555555555555ULL;
 
 // special move related
-constexpr B64 WHITE_CASTLE = ROW_1;
-constexpr B64 BLACK_CASTLE = ROW_8;
+constexpr B64 WHITE_CASTLE_ROW = ROW_1;
+constexpr B64 BLACK_CASTLE_ROW = ROW_8;
+constexpr B64 VOID_WHITE_CASTLE = ~WHITE_CASTLE_ROW;
+constexpr B64 VOID_BLACK_CASTLE = ~BLACK_CASTLE_ROW;
+
+constexpr B64 LONG_CASTLE_KING_COLUMN = COLUMN_C;
+constexpr B64 LONG_CASTLE_ROOK_COLUMN = COLUMN_D;
+constexpr B64 SHORT_CASTLE_KING_COLUMN = COLUMB_G;
+constexpr B64 SHORT_CASTLE_ROOK_COLUMN = COLUMB_F;
+
 // the row to which white writes en-passants
 constexpr B64 WHITE_EN_PASSANT = ROW_3;
 // the row to which black writes en-passants
 constexpr B64 BLACK_EN_PASSANT = ROW_6;
 constexpr B64 ALL_EN_PASSANT = WHITE_EN_PASSANT | BLACK_EN_PASSANT;
-constexpr B64 VOID_WHITE_CASTLE = ~WHITE_CASTLE;
-constexpr B64 VOID_BLACK_CASTLE = ~BLACK_CASTLE;
 constexpr B64 VOID_ALL_EN_PASSANT = ~ALL_EN_PASSANT;
 
 constexpr int PAWN_INITIAL_ROW = 2;
