8b3238c97eec42a83c91d1171e4e9439209a780f
Jonathan Braver <enatanb@gmail.com>
2026-02-12 21:12:10 +0700

    comment update

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -8,10 +8,7 @@ int main() {
   std::string startFen =
       "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-  std::cout << "=== PERFT VALIDATION TEST ===" << std::endl;
-
-  // Test with correct expected values
-  std::cout << "\n--- Test 1: Depth 4 ---" << std::endl;
+  // Depth 4
   PerftStats expected4;
   expected4.nodes = 197281;
   expected4.captures = 1576;
@@ -22,8 +19,7 @@ int main() {
   expected4.checkmates = 8;
   runDetailedPerftTest(startFen, 4, &expected4);
 
-  // Test with incorrect expected values to show validation works
-  std::cout << "\n--- Test 2: Depth 5 ---" << std::endl;
+  // Depth 5
   PerftStats expected5;
   expected5.nodes = 4865609;
   expected5.captures = 82719;
