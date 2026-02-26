d8d20e86aee0973ef19f5c473e8a450bf061ea41
Jonathan Braver <Enatanb@gmail.com>
2026-02-26 09:36:04 +0200

    seperated material eval to its own function

--- Source/Evaluation.cpp
+++ Source/Evaluation.cpp
@@ -1,21 +1,19 @@
 #include "Evaluation.h"
 #include "EvaluationStructure.h"
 
+int Evaluator::evaluateMaterial(const Position& pos, Color color) {
+    int score = 0;
+    score += pos.getPieces(color, PieceType::PAWN).countSetBits() * CENTIPAWNS.pawn_value;
+    score += pos.getPieces(color, PieceType::KNIGHT).countSetBits() * CENTIPAWNS.knight_value;
+    score += pos.getPieces(color, PieceType::BISHOP).countSetBits() * CENTIPAWNS.bishop_value;
+    score += pos.getPieces(color, PieceType::ROOK).countSetBits() * CENTIPAWNS.rook_value;
+    score += pos.getPieces(color, PieceType::QUEEN).countSetBits() * CENTIPAWNS.queen_value;
+    return score;
+}
+
 int Evaluator::evaluate(const Position& pos) {
-    int ownScore = 0;
-    int opponentScore = 0;
-
-    ownScore += pos.getOwnPieces(PieceType::PAWN).countSetBits() * CENTIPAWNS.pawn_value;
-    ownScore += pos.getOwnPieces(PieceType::KNIGHT).countSetBits() * CENTIPAWNS.knight_value;
-    ownScore += pos.getOwnPieces(PieceType::BISHOP).countSetBits() * CENTIPAWNS.bishop_value;
-    ownScore += pos.getOwnPieces(PieceType::ROOK).countSetBits() * CENTIPAWNS.rook_value;
-    ownScore += pos.getOwnPieces(PieceType::QUEEN).countSetBits() * CENTIPAWNS.queen_value;
-
-    opponentScore += pos.getOpponentPieces(PieceType::PAWN).countSetBits() * CENTIPAWNS.pawn_value;
-    opponentScore += pos.getOpponentPieces(PieceType::KNIGHT).countSetBits() * CENTIPAWNS.knight_value;
-    opponentScore += pos.getOpponentPieces(PieceType::BISHOP).countSetBits() * CENTIPAWNS.bishop_value;
-    opponentScore += pos.getOpponentPieces(PieceType::ROOK).countSetBits() * CENTIPAWNS.rook_value;
-    opponentScore += pos.getOpponentPieces(PieceType::QUEEN).countSetBits() * CENTIPAWNS.queen_value;
+    int ownScore = evaluateMaterial(pos, pos.getCurrentColor());
+    int opponentScore = evaluateMaterial(pos, pos.getOpponentColor());
 
     return ownScore - opponentScore;
 }
--- Source/Evaluation.h
+++ Source/Evaluation.h
@@ -2,6 +2,9 @@
 #include "Position.h"
 
 class Evaluator {
+private:
+    static int evaluateMaterial(const Position& pos, Color color);
+
 public:
     // Static evaluation of the current position from the perspective of the side to move.
     static int evaluate(const Position& pos);
